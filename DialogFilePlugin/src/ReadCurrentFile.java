cfb3dbcbdf1acc50edc32f018fa3c4f555de0189
pamepeixinho <pameiupip@gmail.com>
2017-03-26 23:12:18 -0300

    using PSI

--- DialogFilePlugin/src/ReadCurrentFile.java
+++ DialogFilePlugin/src/ReadCurrentFile.java
@@ -1,10 +1,22 @@
+import com.intellij.ide.highlighter.JavaFileType;
+import com.intellij.lang.ASTNode;
+import com.intellij.lang.FileASTNode;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.LangDataKeys;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.fileEditor.FileEditorManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
+import com.intellij.psi.PsiDocumentManager;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiLocalVariable;
+import com.intellij.psi.tree.TokenSet;
+import com.intellij.psi.tree.xml.IDTDElementType;
+import com.sun.tools.internal.ws.processor.model.java.JavaType;
+import com.sun.tools.javac.model.JavacTypes;
+import myToolWindow.SimpleToolWindowFactory;
 
 
 /**
@@ -18,6 +30,8 @@ public class ReadCurrentFile extends AnAction {
 	public void actionPerformed(AnActionEvent e) {
 
 		final Project project = e.getProject();
+
+
 
 		String txt = Messages.showInputDialog(project, "What is your Doubt?", "Input Your Query", Messages.getQuestionIcon());
 //		Messages.showMessageDialog(project, "Hello, " + txt + "!\n I am glad to see you.", "Information", Messages.getInformationIcon());
@@ -40,6 +54,18 @@ public class ReadCurrentFile extends AnAction {
 		if (document == null) {
 			return;
 		}
+//---------TESTING extract PSI
+//		PsiFile teste = e.getData(LangDataKeys.PSI_FILE);
+		PsiFile psiFile = PsiDocumentManager.getInstance(project).getPsiFile(document);
+
+		assert psiFile != null;
+		System.out.println(psiFile.getLanguage());
+		System.out.println(psiFile.getFileType());
+		FileASTNode astFileNodes = psiFile.getNode();
+		ASTNode node = astFileNodes.getFirstChildNode().getTreeNext();
+		System.out.println(node);
+//---------------------------
+//		JavaFileType.INSTANCE;
 
 		String currentText = document.getText();
 		System.out.print(currentText);
